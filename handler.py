4b5f8baf2b4cafb0de374b7e2f7ea7bebd969783
KUB88 <718746159@qq.com>
2016-04-07 15:46:33 +0800

    select successfully and rewrite some other function

--- handler.py
+++ handler.py
@@ -1,6 +1,7 @@
 import re
+from sql import *
 
-def handle_arp(file = 'C:\\Users\\yangh\\Desktop\\py\\showHost\\conf\\arp.txt'):
+def handle_arp( DB, SWITCHER, file = 'C:\\Users\\yangh\\Desktop\\py\\showHost\\conf\\arp.txt'):
     L = []
     with open(file, 'r') as f:
         for line in f.readlines():
@@ -8,24 +9,39 @@ def handle_arp(file = 'C:\\Users\\yangh\\Desktop\\py\\showHost\\conf\\arp.txt'):
             macre = re.search("([0-9a-zA-Z]{4}\.){2}[0-9a-zA-Z]{4}", line)
             if not macre:
                 print('[info]ip address: '+ipre.group()+' has Incomplete mac address')
+                DB._insert(ipre.group(),'','',SWITCHER,'mac Incomplete')
                 continue
             else:
                 d = {'ip':ipre.group(),'mac':macre.group()}
                 L.append(d)
     return L
 
-def handle_port(L, file = 'C:\\Users\\yangh\\Desktop\\py\\showHost\\conf\\mac.txt'):
+def handle_port(L, DB ,SWITCHER, file = 'C:\\Users\\yangh\\Desktop\\py\\showHost\\conf\\mac.txt'):
     for row in L:
         with open(file, 'r') as f:
             for line in f.readlines():
                 macre = re.search("([0-9a-zA-Z]{4}\.){2}[0-9a-zA-Z]{4}", line)
-                portre = re.search("[PGFTRS].+[a-z0-9]$", line)
+                portre = re.search("[PGFTECRS].+[a-z0-9]$", line)
                 if row['mac'] == macre.group():
-                    if portre:
+                    if portre and portre.group() != 'Router':
                         print('ip address: '+row['ip']+' mac address: '+row['mac']+' port: '+portre.group())
+                        DB._insert(row['ip'],row['mac'],portre.group(),SWITCHER,'')
+                        continue
+                    if not portre:
+                        print('[info]ip address: '+row['ip']+' mac address: '+row['mac']+' no entries in mac table')
+                        DB._insert(row['ip'],row['mac'],'',SWITCHER,'no entries in mac table')
+                        continue
                     else:
-                        print('[info]ip address: '+row['ip']+' mac address: '+row['mac']+' has no port!')
+                        continue
+def check(ip, mac):
+    #res = select ip mac
+    if res:
+        return True
+    else:
+        return False
 
-
-arplist = handle_arp()
-handle_port(arplist)
+if __name__ == '__main__':
+    DB = sql()
+    arplist = handle_arp(DB,1)
+    handle_port(arplist,DB,1)
+    DB._commit()
--- sql.py
+++ sql.py
@@ -1,51 +1,63 @@
 import sqlite3
 
+class sql(object):      
+    def __init__(self):
+        try:
+            self.con = sqlite3.connect('C:\\Users\\yangh\\Desktop\\py\\showHost\\showHost.db')
+            print('connect to db successfully')
+        except Exception:
+            print('failed to connect to db')
+            exit(0)
+
+    def create_table(self):
+        self.con.execute('''
+            CREATE TABLE HOST
+           (ID INTEGER PRIMARY KEY AUTOINCREMENT     NOT NULL,
+           IP             CHAR(50)    NOT NULL,
+           PORT           CHAR(50),
+           MAC            CHAR(50),
+           SWITCHER       CHAR(50),
+           BACKUP         CHAR(50),
+           TIME           TimeStamp NOT NULL DEFAULT CURRENT_TIMESTAMP);
+           ''')
+        print('create table successfully')
         
-def connect_db():
-    try:
-        con = sqlite3.connect('C:\\Users\\yangh\\Desktop\\py\\showHost\\showHost.db')
-        print('connect to db successfully')
-        return con
-    except Exception:
-        print('failed to connect to db')
-        exit(0)
 
-def create_table():
-    con = connect_db()
-    con.execute('''
-        CREATE TABLE HOST
-       (ID INTEGER PRIMARY KEY AUTOINCREMENT     NOT NULL,
-       IP             CHAR(50)    NOT NULL,
-       PORT           CHAR(50)     NOT NULL,
-       MAC            CHAR(50)     NOT NULL);
-       ''')
-    print('create table successfully')
-    con.close()
+    def _insert(self, ipadd, interfaceport, macadd, switcher, backup, table = 'HOST'):
+        self.con.execute("INSERT INTO " + table + " (IP,PORT,MAC,SWITCHER,BACKUP) \
+          VALUES ( ?, ?, ?, ?, ? )",(ipadd, interfaceport, macadd, switcher, backup));
+        print('insert successfully')
+    
+    def _commit(self):
+        self.con.commit()
+        
 
-def _insert(ipadd, interfaceport, macadd, table = 'HOST'):
-    con = connect_db()
-    con.execute("INSERT INTO " + table + " (IP,PORT,MAC) \
-      VALUES ( ?, ?, ? )",(ipadd, interfaceport, macadd));
-    con.commit()
-    print('insert successfully')
-    con.close()
+    def _select(self,  method='AND',**kwargs):
+        condition = []
+        if len(kwargs.keys()) == 0:
+            print('please give paraments')
+            exit(0)
+        for i in kwargs.keys():
+            valus = "\'"+str(kwargs.get(i))+"\'"
+            condition.append(str(i)+'='+valus)
+        if method == 'AND':
+            a=' AND '
+        if method == 'OR':
+            a=' OR '
+        condition = str(a.join(condition))
+        print(condition)
+        res = self.con.execute("SELECT * FROM HOST WHERE "+condition)
+        return res
 
-def _select():
-    con = connect_db()
-    res = con.execute("SELECT * FROM HOST")
-    for row in res:
-        print(row)
-
-def _delete(id, table = 'HOST'):
-    con = connect_db()
-    con.execute('DELETE FROM '+ table +' where ID =' + id )
-    print('delete row '+id+' successfully')
-    con.commit()
-    con.close    
-
-#create_table()
-#_insert('1.1.1.1', 'gi1/1', '6666-6666-6666')
-#_delete('11')
-#_select()
+    def _delete(self, id, table = 'HOST'):
+        self.con.execute('DELETE FROM '+ table +' where ID =' + id )
+        print('delete row '+id+' successfully')
+        self.con.commit()
+            
+a=sql()
+res = a._select(BACKUP='1')
+for row in res:
+    print(row)
+#a.create_table()
     
     
